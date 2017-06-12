f3f2a3bf3c9330924bb4c1ea9c5c2963062597e0
KiboSoremda <obsidiankid@yahoo.com>
2017-06-12 12:07:24 -0700

    Create HelloWorld.cpp

---
+++ DWYW/HelloWorld.cpp
@@ -0,0 +1,10 @@
+/* the basic "Hello, World!" program. */
+
+#include <iostream> // Input/Output stream
+
+using namespace std; //uses standard namespace
+
+int main() {
+
+	cout << "Hello, Period 7 APCS! :)" << endl;
+}
